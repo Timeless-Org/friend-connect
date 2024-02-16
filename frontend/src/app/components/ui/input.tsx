f41787f57b675daa19db33f5327c0366d34ae0d1
cardene <chotips08@gmail.com>
2024-02-17 00:34:44 +0900

    #11 💫 add lib dir

--- frontend/src/app/components/ui/input.tsx
+++ frontend/src/app/components/ui/input.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/libs/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       />
     );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
--- frontend/src/app/libs/PrivyWagmiConfig.tsx
+++ frontend/src/app/libs/PrivyWagmiConfig.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { blastSepolia } from "@/libs/chain";
+import { PrivyWagmiConnector } from "@privy-io/wagmi-connector";
 import React from "react";
-import { blastSepolia } from "@lib/chain";
 import { configureChains } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { PrivyWagmiConnector } from "@privy-io/wagmi-connector";
 
 const config = configureChains([blastSepolia], [publicProvider()]);
 
-export default function PrivyWagmiConnectorComponent({ children }: { children: React.ReactNode }) {
+export default function PrivyWagmiConnectorComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <PrivyWagmiConnector wagmiChainsConfig={config}>
       {children}
